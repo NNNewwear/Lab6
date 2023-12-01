7f3201205242400ffed0a738a4235f3a07f4d2a2
NNNewwear <nextkantapong@gmail.com>
2023-12-01 14:10:12 +0700

    lab6_1.cpp completed

--- lab6_1.cpp
+++ lab6_1.cpp
@@ -1,9 +1,30 @@
 #include<iostream>
 using namespace std;
 
-int main(){
-    cout << "Enter an integer: ";
-    cout << "#Even numbers = ";
-    cout << "#Odd numbers = ";
+int main()
+{
+    int i[100],j=0;
+    do
+    {
+        cout << "Enter an integer: ";
+        cin>>i[j];
+        j++;
+    }
+    while(i[j-1]!=0);
+    int x=0,y=0;
+    for(int k=0; k<j-1; k++)
+    {
+        if(i[k]%2==0)
+        {
+            x++;
+        }
+        else
+        {
+            y++;
+        }
+       
+    }
+    cout << "#Even numbers = "<<x<<endl;
+    cout << "#Odd numbers = "<<y;
     return 0;
 }
